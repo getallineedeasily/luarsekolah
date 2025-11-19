bcab8fede2869e0a3bb7b3640290087a2b1c6a72
ricky <ricky>
2025-11-19 10:38:09 +0700

    update: add program image seeder

--- database/seeders/ProgramSeeder.php
+++ database/seeders/ProgramSeeder.php
@@ -22,7 +22,8 @@ class ProgramSeeder extends Seeder
                     'Mahir dalam Pencatatan Transaksi dan Jurnal Umum',
                     'Keterampilan dalam Menyusun Laporan Keuangan',
                     'Keahlian dalam Analisis Lanjutan dan Pengambilan Keputusan'
-                ]
+                ],
+                'image' => 'https://pbi.belajarbekerja.com/upload/project/gambar_project/afb76a4a6bfb7b237548ee4ae46691c6.png'
             ],
             [
                 'name' => 'Content Writing',
@@ -31,7 +32,8 @@ class ProgramSeeder extends Seeder
                     'Kuasai Teknik Menulis yang Efektif',
                     'Optimasi SEO',
                     'Portofolio yang Kuat'
-                ]
+                ],
+                'image' => 'https://pbi.belajarbekerja.com/upload/project/gambar_project/b78b711845bab16b1cb07372e0da86a9.png'
             ],
             [
                 'name' => 'Data Analytic',
@@ -40,7 +42,8 @@ class ProgramSeeder extends Seeder
                     'Real Case dengan Big Data',
                     'Kuasai Microsoft Excel dan Dashboard',
                     'Mengembangkan Data Storytelling'
-                ]
+                ],
+                'image' => 'https://pbi.belajarbekerja.com/upload/project/gambar_project/37acaf2f68e69f70272d6d565f20c202.png'
             ],
             [
                 'name' => 'Digital Marketing',
@@ -49,7 +52,8 @@ class ProgramSeeder extends Seeder
                     'Tingginya Permintaan Industri',
                     'Kuasai Ads, Social Media dan Copywriting',
                     'Pahami Pasar dan Kompetitor'
-                ]
+                ],
+                'image' => 'https://pbi.belajarbekerja.com/upload/project/gambar_project/b6305eb2936b81e08de816fa8f3ad4ac.png'
             ],
             [
                 'name' => 'Graphic Design',
@@ -58,7 +62,8 @@ class ProgramSeeder extends Seeder
                     'Kuasai Software Desain Profesional',
                     'Tingkatkan Kreativitas dan Problem Solving Visual',
                     'Portofolio Desain yang Menarik Perusahaan'
-                ]
+                ],
+                'image' => 'https://pbi.belajarbekerja.com/upload/project/gambar_project/9c49b7e5f764e115c8519178b7e95972.png'
             ],
             [
                 'name' => 'Marketing Communication',
@@ -67,7 +72,8 @@ class ProgramSeeder extends Seeder
                     'Menguasai Strategi IMC yang Efektif',
                     'Keahlian dalam Branding dan Digital Marketing',
                     'Analisis Marketing Campaign'
-                ]
+                ],
+                'image' => 'https://pbi.belajarbekerja.com/upload/project/gambar_project/c8638dd701e39b3fb413c7c8614b97aa.png'
             ],
             [
                 'name' => 'Social Media Marketing',
@@ -76,7 +82,8 @@ class ProgramSeeder extends Seeder
                     'Pahami Algoritma Media Sosial',
                     'Buat Kampanye yang Menarik',
                     'Kembangkan Strategi Media Sosial'
-                ]
+                ],
+                'image' => 'https://pbi.belajarbekerja.com/upload/project/gambar_project/c8638dd701e39b3fb413c7c8614b97aa.png'
             ],
             [
                 'name' => 'Software Engineering (Mobile)',
@@ -85,7 +92,8 @@ class ProgramSeeder extends Seeder
                     'Kuasai Flutter untuk Pengembangan Aplikasi Mobile',
                     'Integrasi Backend dan Keamanan Aplikasi',
                     'Kemampuan dalam Pengujian dan Manajemen Vers'
-                ]
+                ],
+                'image' => 'https://pbi.belajarbekerja.com/upload/project/gambar_project/dddd25b2598e9f820b9a22e96571076e.png'
             ],
             [
                 'name' => 'UI/UX Design',
@@ -94,7 +102,8 @@ class ProgramSeeder extends Seeder
                     'Tingginya Permintaan Industri',
                     'Keterampilan Prototyping dan Wireframing',
                     'Pahami Kebutuhan User'
-                ]
+                ],
+                'image' => 'https://pbi.belajarbekerja.com/upload/project/gambar_project/3ea8b706736a50eee9ed80ea0f640c56.png'
             ],
             [
                 'name' => 'Web Development',
@@ -103,7 +112,8 @@ class ProgramSeeder extends Seeder
                     'Kuasai Bahasa Pemrograman dan Modern Framework',
                     'Optimalkan Website untuk SEO',
                     'Kemampuan Pengembangan Website'
-                ]
+                ],
+                'image' => 'https://pbi.belajarbekerja.com/upload/project/gambar_project/4f711692fa2231e8cb0c4a0965594298.png'
             ],
         ];
 
