6ac8618aa0de842b9c6bc82d493d5180ceecabed
ricky <ricky>
2025-11-28 10:41:20 +0700

    update hero section

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -6,20 +6,55 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Magang Oleh Luarsekolah</title>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
+
+    {{-- Fonts --}}
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+        rel="stylesheet">
+
 </head>
 
-<body>
+<body class="font-poppins">
     <header></header>
     <main>
-
         {{-- Hero --}}
-        <section>
-            <h1></h1>
-            <h2></h2>
-            <button></button>
-            <span></span>
-            <div>
-                {{-- carousel of program available --}}
+        <section class="bg-[#0F0F0F] text-white h-dvh pt-30">
+            <div class="max-w-3/4 mx-auto">
+                <div class="w-[547px]">
+                    <h1 class="text-[42px] font-medium">Belajar Langsung dari Praktisi Berbagai Industri - bukan
+                        Sekedar Teori!
+                    </h1>
+                    <h5 class="text-sm pb-6">Dibimbing mentor profesional dari berbagai Industri & real
+                        project
+                    </h5>
+                </div>
+                <div class="space-x-2.5 mb-18">
+                    <a class="inline-block text-center text-sm bg-[#FFD633] rounded-md p-2.5 w-[162px] h-[43px] text-black leading-[1.75]"
+                        href="#">Daftar Sekarang</a>
+                    <a class="inline-block text-center text-sm rounded-md p-2.5 w-[162px] h-[43px] border leading-[1.75]"
+                        href="#">Lihat
+                        Mentor</a>
+                </div>
+                <div class="grid grid-cols-4 text-center">
+                    <div class="flex flex-col">
+                        <span class="text-[42px] font-medium leading-none">700+</span>
+                        <span class="text-base font-medium">Talenta Bergabung</span>
+                    </div>
+                    <div class="flex flex-col">
+                        <span class="text-[42px] font-medium leading-none">100+</span>
+                        <span class="text-base font-medium">Portofolio Tersedia</span>
+                    </div>
+                    <div class="flex flex-col">
+                        <span class="text-[42px] font-medium leading-none">88%</span>
+                        <span class="text-base font-medium">Kepuasan Talenta</span>
+                    </div>
+                    <div class="flex flex-col">
+                        <span class="text-[42px] font-medium leading-none">20+</span>
+                        <span class="text-base font-medium">Universitas & Ormawa Bekerjasama</span>
+                    </div>
+                </div>
             </div>
         </section>
     </main>
