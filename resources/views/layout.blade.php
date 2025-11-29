4565622ac40e0b7155dc2f3c1f4bd973d14f4419
ricky <ricky>
2025-11-29 12:12:32 +0700

    update program section

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -58,7 +58,7 @@
             </div>
         </section>
 
-        {{-- List PBI --}}
+        {{-- List Program PBI --}}
         <section class="pt-30">
             <div class="max-w-10/12 mx-auto">
                 <h3 class="font-medium text-2xl text-[#0F0F0F] text-center pb-2">Project-Based Internship</h3>
@@ -67,23 +67,27 @@
                 </h5>
                 <div class="flex mt-8 gap-6 overflow-x-scroll mb-10">
                     @foreach ($programs as $program)
-                        <div class="rounded-[10px] shadow-[0px_2px_6px_0px_rgba(0,0,0,0.08)] w-[247px] shrink-0">
+                        <div
+                            class="rounded-[10px] shadow-[0px_2px_6px_0px_rgba(0,0,0,0.08)] w-[247px] shrink-0 flex flex-col">
                             <div class="">
                                 <img class="rounded-t-[10px]" src="{{ $program->image }}" alt="">
                             </div>
-                            <div class="p-4">
-                                <h4 class="text-[#1E1E1E] font-medium mb-4">
-                                    {{ $program->name }}
-                                </h4>
-                                <p class="text-sm text-[#4F4F4F]">Luna Yofanda -
-                                </p>
-                                <p class="text-xs text-[#4F4F4F] mb-4">Accounting and Finance
-                                    Associate at Shopee
-                                    Indonesia</p>
-                                <p class="text-sm font-medium text-[#4F4F4F] mb-8">Berpengalaman 5 tahun dalam bidangnya
-                                </p>
-                                <a class="inline-block text-center text-sm bg-[#FFD633] rounded-md p-2.5 h-[43px] text-black leading-[1.75] w-full"
-                                    href="#">Lihat Detail Program</a>
+                            <div class="p-4 h-full flex flex-col justify-between">
+                                <div>
+                                    <h4 class="text-[#1E1E1E] font-medium mb-4">
+                                        {{ $program->name }}
+                                    </h4>
+                                    <p class="text-sm text-[#4F4F4F]">{{ $program->mentor }} -
+                                    </p>
+                                    <p class="text-xs text-[#4F4F4F] mb-4">{{ $program->mentor_job }}</p>
+                                    <p class="text-sm font-medium text-[#4F4F4F] mb-8">Berpengalaman 5 tahun dalam
+                                        bidangnya
+                                    </p>
+                                </div>
+                                <div>
+                                    <a class="inline-block text-center text-sm bg-[#FFD633] rounded-md p-2.5 h-[43px] text-black leading-[1.75] w-full"
+                                        href="#">Lihat Detail Program</a>
+                                </div>
                             </div>
                         </div>
                     @endforeach
@@ -104,7 +108,7 @@
                         </svg></a>
                 </div>
 
-                <div class="bg-[#FFCC00] rounded-[10px] p-4 space-y-2.5 shadow-[0px_8px_16px_0px_rgba(0,0,0,0.08)]">
+                <div class="bg-[#FFCC00] rounded-[10px] p-5 space-y-2.5 shadow-[0px_8px_16px_0px_rgba(0,0,0,0.08)]">
                     <h2 class="font-medium text-[28px] text-[#0F0F0F] w-1/2">Masih ragu ? Kenali dulu mentornya
                         & rasakan value-nya!</h2>
                     <span class="text-sm text-[#3D3D3D] block mb-5">No worries, coba mini PBI yang langsung dibimbing
