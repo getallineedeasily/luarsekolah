e4542b6372fd99d51cb898bf747539eee8caf30e
ricky <ricky.gunawan.ti.22@cic.ac.id>
2025-12-02 22:40:00 +0700

    update faq db (#27)
    Co-authored-by: ricky <ricky>

--- app/Models/Faq.php
+++ app/Models/Faq.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Faq extends Model
+class FAQ extends Model
 {
     use HasFactory;
 
