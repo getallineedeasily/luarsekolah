f24ee07311cb00b6ee488627ca9d1fa80f8a0fe9
ricky <ricky.gunawan.ti.22@cic.ac.id>
2025-12-03 01:14:31 +0700

    refactor to blade component (#29)
    Co-authored-by: ricky <ricky>

---
+++ resources/views/components/bottom-footer.blade.php
@@ -0,0 +1,8 @@
+<div class="border-t pt-20">
+    <p class="font-medium text-[15px]">Belajar Bekerja merupakan program untuk mempersiapkan individu
+        menjadi lebih siap secara
+        profesional. Dimana peserta dapat mengerjakan kasus nyata yang terjadi di dunia industri dengan
+        memanfaatkan pemahaman digital transformasi serta teknologi Kecerdasan Buatan (AI) yang langsung
+        didampingi oleh praktisi berpengalaman. Pada program ini semua orang memiliki kesempatan untuk
+        berpengalaman di industri!</p>
+</div>
