3b328f960b5b1f081d1fdd97c2819afdfaf1b5b4
ricky <ricky.gunawan.ti.22@cic.ac.id>
2025-12-03 02:27:21 +0700

    update footer (#31)
    Co-authored-by: ricky <ricky>

--- resources/views/components/bottom-footer.blade.php
+++ resources/views/components/bottom-footer.blade.php
@@ -1,8 +1,10 @@
-<div class="border-t pt-20">
-    <p class="font-medium text-[15px]">Belajar Bekerja merupakan program untuk mempersiapkan individu
+<div class="border-t pt-6 font-medium text-[15px]">
+    <p class="mb-6">&copy; 2025 Belajar Bekerja. All rights reserved.</p>
+    <p>Belajar Bekerja merupakan program untuk mempersiapkan individu
         menjadi lebih siap secara
         profesional. Dimana peserta dapat mengerjakan kasus nyata yang terjadi di dunia industri dengan
         memanfaatkan pemahaman digital transformasi serta teknologi Kecerdasan Buatan (AI) yang langsung
         didampingi oleh praktisi berpengalaman. Pada program ini semua orang memiliki kesempatan untuk
-        berpengalaman di industri!</p>
+        berpengalaman di industri!
+    </p>
 </div>
--- resources/views/components/top-footer.blade.php
+++ resources/views/components/top-footer.blade.php
@@ -1,38 +1,140 @@
- <div class="pb-28 grid grid-cols-5 gap-10">
-     <div class="space-y-10">
-         <div>
-             <img src="{{ asset('images/Logo_BB_Putih.png') }}" alt="Logo_BB_Putih">
-         </div>
-         <p class="font-medium text-[15px]">Headquarter Centennial Tower Level 29, Jl Jend Gatot Subroto
-             No.27,
-             Karet Semanggi, Setiabudi Daerah Khusus Ibukota Jakarta 12950</p>
-     </div>
-     <div class="flex flex-col gap-4">
-         <p class="text-[15px] font-medium">Produk</p>
-         <a class="text-[15px] font-medium underline" href="#">Luarsekolah</a>
-         <a class="text-[15px] font-medium underline" href="#">Project Based Internship (PBI)</a>
-         <a class="text-[15px] font-medium underline" href="#">Belajar Bekerja Bootcamp</a>
-         <a class="text-[15px] font-medium underline" href="#">Aplikasi <span
-                 class="text-[#0071FF]">segera</span></a>
-     </div>
-     <div class="flex flex-col gap-4">
-         <p class="text-[15px] font-medium">Perusahaan</p>
-         <a class="text-[15px] font-medium underline" href="#">Tentang Kami</a>
-         <a class="text-[15px] font-medium underline" href="#">FAQ</a>
-         <a class="text-[15px] font-medium underline" href="#">Gabung Komunitas</a>
-         <a class="text-[15px] font-medium underline" href="#">Blog</span></a>
-     </div>
-     <div class="flex flex-col gap-4">
-         <p class="text-[15px] font-medium">Media Sosial</p>
-         <a class="text-[15px] font-medium underline" href="#">Instagram</a>
-         <a class="text-[15px] font-medium underline" href="#">Youtube</a>
-         <a class="text-[15px] font-medium underline" href="#">TikTok</a>
-         <a class="text-[15px] font-medium underline" href="#">LinkedIn</span></a>
-     </div>
-     <div class="flex flex-col gap-4">
-         <p class="text-[15px] font-medium">Metode Pembayaran</p>
-         <a class="text-[15px] font-medium underline" href="#">Partner</a>
-         <a class="text-[15px] font-medium underline" href="#">Kartu Kredit</a>
-         <a class="text-[15px] font-medium underline" href="#">Bank Transfer</a>
-     </div>
- </div>
+<div class="pb-20 grid grid-cols-5 gap-10">
+    <div class="space-y-8">
+        <div>
+            <img src="{{ asset('images/Logo_BB_Putih.png') }}" alt="Logo_BB_Putih">
+        </div>
+        <p class="font-medium text-[15px]">Headquarter Centennial Tower Level 29, Jl Jend Gatot Subroto
+            No.27,
+            Karet Semanggi, Setiabudi Daerah Khusus Ibukota Jakarta 12950</p>
+        <div class="flex justify-start gap-4 items-center">
+            <aside class="size-8">
+                <svg width="26" height="26" viewBox="0 0 26 26" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path fill-rule="evenodd" clip-rule="evenodd"
+                        d="M22.2117 3.77542C19.7679 1.34202 16.5177 0.0012346 13.0546 0C5.91868 0 0.111111 5.77487 0.108628 12.8732C0.107387 15.1424 0.703911 17.3573 1.83675 19.3092L0 25.981L6.86282 24.1908C8.75355 25.2168 10.8827 25.7569 13.049 25.7576H13.0546C20.1893 25.7576 25.9975 19.9821 26 12.8837C26.0012 9.44353 24.6561 6.20945 22.2117 3.77604V3.77542ZM13.0546 23.5834H13.0503C11.1198 23.5829 9.22596 23.0667 7.57358 22.092L7.18064 21.86L3.10801 22.9223L4.19491 18.9734L3.93917 18.5685C2.8622 16.8647 2.29298 14.8955 2.29423 12.8739C2.29671 6.97429 7.12352 2.17414 13.059 2.17414C15.9329 2.17537 18.6344 3.28961 20.666 5.31251C22.6977 7.3348 23.8156 10.0238 23.8144 12.8825C23.8119 18.7827 18.9851 23.5829 13.0546 23.5829V23.5834ZM18.9565 15.5696C18.6331 15.4085 17.0429 14.6307 16.7461 14.5233C16.4494 14.4159 16.234 14.3622 16.0186 14.6844C15.8032 15.0067 15.1831 15.7314 14.9944 15.9455C14.8057 16.1604 14.617 16.1869 14.2936 16.0258C13.9702 15.8647 12.928 15.5252 11.6921 14.4294C10.7306 13.5763 10.0813 12.5232 9.89264 12.201C9.70392 11.8788 9.87278 11.7047 10.0341 11.5448C10.1794 11.4004 10.3575 11.1689 10.5195 10.9812C10.6816 10.7935 10.735 10.659 10.8429 10.4447C10.951 10.2299 10.897 10.0423 10.8163 9.88115C10.7355 9.72005 10.0888 8.13663 9.81876 7.49281C9.55616 6.86564 9.28929 6.95083 9.09126 6.94034C8.90254 6.93108 8.68718 6.92923 8.47116 6.92923C8.25514 6.92923 7.90501 7.00948 7.60832 7.33171C7.31163 7.65391 6.4761 8.43233 6.4761 10.0151C6.4761 11.5979 7.63504 13.1282 7.79704 13.343C7.95903 13.5579 10.0782 16.8067 13.3228 18.2006C14.0944 18.532 14.6971 18.7302 15.167 18.8784C15.9417 19.1234 16.6468 19.0889 17.2042 19.0061C17.8256 18.9136 19.1179 18.2277 19.3873 17.4765C19.6567 16.7252 19.6567 16.0807 19.5761 15.9468C19.4954 15.8128 19.2794 15.7319 18.956 15.5708L18.9565 15.5696Z"
+                        fill="#111B21" />
+                </svg>
+            </aside>
+            <div class="font-medium text-[15px] text-[#1E1E1E]">
+                <p class="">Whatsapp</p>
+                <p class="underline">+62 811-2021-444</p>
+            </div>
+        </div>
+        <div class="flex justify-start gap-4 items-center">
+            <aside class="size-8">
+                <svg width="27" height="22" viewBox="0 0 27 22" fill="none"
+                    xmlns="http://www.w3.org/2000/svg">
+                    <path
+                        d="M26.6667 2.66667C26.6667 1.2 25.4667 0 24 0L2.66667 0C1.2 0 0 1.2 0 2.66667L0 18.6667C0 20.1333 1.2 21.3333 2.66667 21.3333H24C25.4667 21.3333 26.6667 20.1333 26.6667 18.6667V2.66667ZM24 2.66667L13.3333 9.33333L2.66667 2.66667L24 2.66667ZM24 18.6667H2.66667L2.66667 5.33333L13.3333 12L24 5.33333V18.6667Z"
+                        fill="black" />
+                </svg>
+            </aside>
+            <div class="font-medium text-[15px] text-[#1E1E1E]">
+                <p class="">Email</p>
+                <p class="underline">info@luarsekolah.com</p>
+            </div>
+        </div>
+        <div class="font-medium text-[15px] text-[#1E1E1E]">
+            <p>Jam pelayanan konsumen</p>
+            <p>(09:00-17:00)</p>
+        </div>
+    </div>
+    <div class="flex flex-col gap-4">
+        <p class="text-[15px] font-medium">Produk</p>
+        <a class="text-[15px] font-medium underline" href="#">Luarsekolah</a>
+        <a class="text-[15px] font-medium underline" href="#">Project Based Internship (PBI)</a>
+        <a class="text-[15px] font-medium underline" href="#">Belajar Bekerja Bootcamp</a>
+        <a class="text-[15px] font-medium underline" href="#">Aplikasi <span
+                class="text-[#0071FF]">segera</span></a>
+    </div>
+    <div class="flex flex-col gap-4">
+        <p class="text-[15px] font-medium">Perusahaan</p>
+        <a class="text-[15px] font-medium underline" href="#">Tentang Kami</a>
+        <a class="text-[15px] font-medium underline" href="#">FAQ</a>
+        <a class="text-[15px] font-medium underline" href="#">Gabung Komunitas</a>
+        <a class="text-[15px] font-medium underline" href="#">Blog</span></a>
+    </div>
+    <div class="flex flex-col gap-4">
+        <p class="text-[15px] font-medium">Media Sosial</p>
+        <div class="flex justify-start gap-2 items-center">
+            <aside class="size-5">
+                <svg width="20" height="20" viewBox="0 0 20 20" fill="none"
+                    xmlns="http://www.w3.org/2000/svg">
+                    <path
+                        d="M5.8 0H14.2C17.4 0 20 2.6 20 5.8V14.2C20 15.7383 19.3889 17.2135 18.3012 18.3012C17.2135 19.3889 15.7383 20 14.2 20H5.8C2.6 20 0 17.4 0 14.2V5.8C0 4.26174 0.61107 2.78649 1.69878 1.69878C2.78649 0.61107 4.26174 0 5.8 0ZM5.6 2C4.64522 2 3.72955 2.37928 3.05442 3.05442C2.37928 3.72955 2 4.64522 2 5.6V14.4C2 16.39 3.61 18 5.6 18H14.4C15.3548 18 16.2705 17.6207 16.9456 16.9456C17.6207 16.2705 18 15.3548 18 14.4V5.6C18 3.61 16.39 2 14.4 2H5.6ZM15.25 3.5C15.5815 3.5 15.8995 3.6317 16.1339 3.86612C16.3683 4.10054 16.5 4.41848 16.5 4.75C16.5 5.08152 16.3683 5.39946 16.1339 5.63388C15.8995 5.8683 15.5815 6 15.25 6C14.9185 6 14.6005 5.8683 14.3661 5.63388C14.1317 5.39946 14 5.08152 14 4.75C14 4.41848 14.1317 4.10054 14.3661 3.86612C14.6005 3.6317 14.9185 3.5 15.25 3.5ZM10 5C11.3261 5 12.5979 5.52678 13.5355 6.46447C14.4732 7.40215 15 8.67392 15 10C15 11.3261 14.4732 12.5979 13.5355 13.5355C12.5979 14.4732 11.3261 15 10 15C8.67392 15 7.40215 14.4732 6.46447 13.5355C5.52678 12.5979 5 11.3261 5 10C5 8.67392 5.52678 7.40215 6.46447 6.46447C7.40215 5.52678 8.67392 5 10 5ZM10 7C9.20435 7 8.44129 7.31607 7.87868 7.87868C7.31607 8.44129 7 9.20435 7 10C7 10.7956 7.31607 11.5587 7.87868 12.1213C8.44129 12.6839 9.20435 13 10 13C10.7956 13 11.5587 12.6839 12.1213 12.1213C12.6839 11.5587 13 10.7956 13 10C13 9.20435 12.6839 8.44129 12.1213 7.87868C11.5587 7.31607 10.7956 7 10 7Z"
+                        fill="black" />
+                </svg>
+
+            </aside>
+            <a class="text-[15px] font-medium underline" href="#">Instagram</a>
+        </div>
+
+        <div class="flex justify-start gap-2 items-center">
+            <aside class="size-5">
+                <svg width="21" height="16" viewBox="0 0 21 16" fill="none"
+                    xmlns="http://www.w3.org/2000/svg">
+                    <path d="M8.38892 4.5L14.7222 8L8.38892 11.5V4.5Z" fill="black" />
+                    <path d="M10.5 1C20 1 20 1 20 8C20 15 20 15 10.5 15C1 15 1 15 1 8C1 1 1 1 10.5 1Z" stroke="black"
+                        stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                </svg>
+
+
+            </aside>
+            <a class="text-[15px] font-medium underline" href="#">Youtube</a>
+        </div>
+
+        <div class="flex justify-start gap-2 items-center">
+            <aside class="size-5">
+                <svg width="16" height="18" viewBox="0 0 16 18" fill="none"
+                    xmlns="http://www.w3.org/2000/svg">
+                    <path
+                        d="M12.44 2.82C11.7566 2.03953 11.3799 1.0374 11.38 0H8.29V12.4C8.26666 13.0712 7.98352 13.7071 7.50031 14.1735C7.01709 14.6399 6.3716 14.9004 5.7 14.9C4.28 14.9 3.1 13.74 3.1 12.3C3.1 10.58 4.76 9.29 6.47 9.82V6.66C3.02 6.2 0 8.88 0 12.3C0 15.63 2.76 18 5.69 18C8.83 18 11.38 15.45 11.38 12.3V6.01C12.633 6.90985 14.1374 7.39265 15.68 7.39V4.3C15.68 4.3 13.8 4.39 12.44 2.82Z"
+                        fill="black" />
+                </svg>
+
+            </aside>
+            <a class="text-[15px] font-medium underline" href="#">TikTok</a>
+        </div>
+
+        <div class="flex justify-start gap-2 items-center">
+            <aside class="size-5">
+                <svg width="20" height="18" viewBox="0 0 20 18" fill="none"
+                    xmlns="http://www.w3.org/2000/svg">
+                    <path
+                        d="M4 2.001C3.99974 2.53143 3.78877 3.04004 3.41351 3.41492C3.03825 3.78981 2.52943 4.00027 1.999 4C1.46857 3.99974 0.959965 3.78877 0.585079 3.41351C0.210194 3.03825 -0.000264966 2.52943 2.50361e-07 1.999C0.000265467 1.46857 0.211233 0.959965 0.586494 0.585079C0.961754 0.210194 1.47057 -0.000264966 2.001 2.50361e-07C2.53143 0.000265467 3.04004 0.211233 3.41492 0.586494C3.78981 0.961754 4.00027 1.47057 4 2.001ZM4.06 5.481H0.0600002V18.001H4.06V5.481ZM10.38 5.481H6.4V18.001H10.34V11.431C10.34 7.771 15.11 7.431 15.11 11.431V18.001H19.06V10.071C19.06 3.901 12 4.131 10.34 7.161L10.38 5.481Z"
+                        fill="black" />
+                </svg>
+            </aside>
+            <a class="text-[15px] font-medium underline" href="#">LinkedIn</a>
+        </div>
+
+    </div>
+    <div class="flex flex-col gap-4">
+        <p class="text-[15px] font-medium">Metode Pembayaran</p>
+        <div class="space-y-4">
+            <p class="text-[15px] font-medium">Partner</p>
+            <img class="w-[83px]" src="{{ asset('images/footer/midtrans.png') }}" alt="">
+        </div>
+        <div class="space-y-4">
+            <p class="text-[15px] font-medium">Kartu Kredit</p>
+            <div class="grid grid-cols-3 gap-3">
+                <img class="w-[62px]" src="{{ asset('images/footer/mastercard.png') }}" alt="">
+                <img class="w-[62px]" src="{{ asset('images/footer/visa.png') }}" alt="">
+            </div>
+        </div>
+        <div class="space-y-4">
+            <p class="text-[15px] font-medium">Bank Transfer</p>
+            <div class="grid grid-cols-3 gap-3">
+                <img class="w-[62px]" src="{{ asset('images/footer/bca.png') }}" alt="">
+                <img class="w-[62px]" src="{{ asset('images/footer/bni.png') }}" alt="">
+                <img class="w-[62px]" src="{{ asset('images/footer/bri.png') }}" alt="">
+                <img class="w-[62px]" src="{{ asset('images/footer/gopay.png') }}" alt="">
+                <img class="w-[62px]" src="{{ asset('images/footer/jenius.png') }}" alt="">
+                <img class="w-[62px]" src="{{ asset('images/footer/lainnya.png') }}" alt="">
+            </div>
+        </div>
+        <div class="space-y-3">
+            <p class="text-[15px] font-medium">QRIS</p>
+            <img class="w-[112px]" src="{{ asset('images/footer/qris.png') }}" alt="">
+        </div>
+    </div>
+</div>
